5e8ad0e3b1f516683bca3ef36465cac42dc797eb
jkhwangHi <jghwang11@gmail.com>
2018-03-25 16:21:53 +0900

    pruning - depth minus case

--- snail.cpp
+++ snail.cpp
@@ -1,48 +1,44 @@
 #include <cstdio>
 #include <cstring>
+double cache[1001][1001];
 
-int rainy_day_length, well_depth;
-double cache[1001][2000+2000];
-
-double cal_escape(int date, int depth)
+double cal_escape(int day, int depth)
 {
-	double & ret = cache[date][depth+2000];
+	double & ret = cache[day][depth];
+	
+	if (day == 0)
+		return ret = depth <= 0 ? 1.0 : 0.0;
 
-	if (ret != -1.0)
+	if (depth <= 0)
+		return 1.0;
+
+	if (ret >= 0.0)
 		return ret;
 
-	if (date == 0)
-	{
-		return ret = depth <= 0 ? 1.0 : 0.0;
-	}
-
-	ret = 0.0;
-
-	ret += cal_escape(date - 1, depth - 1) / 4;
-	ret += (3 * cal_escape(date - 1, depth - 2)) / 4;
-
-	return ret ;
+	ret = 1.0;
+	ret = 0.75*cal_escape(day - 1, depth - 2) + 0.25*cal_escape(day - 1, depth - 1);
+	
+	return ret;
 }
 
+void cache_init()
+{
+	for (int i = 0; i < 1001; i++)
+		for (int j = 0; j < 1001; j++)
+			cache[i][j] = -1.0;
+}
 int main()
 {
-	int test_case;
+	int test;
 	freopen("sample.txt", "r", stdin);
-	scanf("%d", &test_case);
-
-	for (int t = 0; t < test_case; t++)
+	scanf("%d", &test);
+	for (int t = 0; t < test; t++)
 	{
-		for (int i = 0; i < 1001; i++)
-		{
-			for (int j = 0; j < 4000; j++)
-			{
-				cache[i][j] = -1.0;
-			}
-		}
+		cache_init();
+		int rainy_day_length, well_depth;
 		scanf("%d %d", &well_depth, &rainy_day_length);
-		
-		printf("%.10f\n", cal_escape(rainy_day_length, well_depth));
-	}
 
+		printf("%.10f\n" , cal_escape(rainy_day_length,well_depth));
+	}
 	return 0;
 }
